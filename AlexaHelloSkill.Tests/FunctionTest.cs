e781a6d3d2c5c80457a7b1754caeb3ee048e391e
Pranav Suresh <nspranav1180@gmail.com>
2017-09-27 14:23:33 -0400

    SendMessageInent

--- AlexaHelloSkill.Tests/FunctionTest.cs
+++ AlexaHelloSkill.Tests/FunctionTest.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+//using Alexa.NET.Request.Type;
 using Xunit;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
+using Amazon.Util;
 
 namespace AlexaHelloSkill.Tests
 {
@@ -18,9 +22,71 @@ namespace AlexaHelloSkill.Tests
             // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
-            var upperCase = function.FunctionHandler("hello world", context);
-       
-            Assert.Equal("HELLO WORLD", upperCase);
+            var upperCase = function.FunctionHandler(GetRequest(), context);
+            
+            Assert.Equal("Sent message to daddy", ((PlainTextOutputSpeech) upperCase.Response.OutputSpeech).Text);
+        }
+
+        private SkillRequest GetRequest()
+        {
+            return new SkillRequest
+            {
+
+                Session = new Session
+                {
+                    New = true,
+                    SessionId = "SessionId.de5aef04-1218-4bcc-b42a-a6c3ccbf4058",
+                    Application = new Application
+                    {
+                        ApplicationId = "amzn1.ask.skill.4320cd87-2196-4cab-a608-431175fd1a84"
+                    },
+                    Attributes = new Dictionary<string, object> { },
+                    User = new User
+                    {
+                        UserId =
+                            "amzn1.ask.account.AHF37SX34WT3GSWLHWPCAREZX6AWL5RM5BE2HU3Y6CELX3VAB7PDMIQHZT3R3OZJHC6BM44NIFBTQ2XKEGPICP4OA5T4JMTNXXVJI6GD3CXGOTYBATWXGIBPN5SYIN6EHOOLRKHSCKRE6POCBFDACIERZJ3WX5IPJJKXYQRTN74XXTUFLYEMR6GH2DWVKHMVLEMMFTWXEJHIUZA"
+                    }
+                },
+                Request = new IntentRequest
+                {
+                    Type = "IntentRequest",
+                    RequestId = "EdwRequestId.ad774ecd-1c84-41f1-80e6-0b334a9927cd",
+                    Intent = new Intent
+                    {
+                        Name = "SendMessageIntent",
+                        Slots = new Dictionary<string, Slot>()
+                        {
+                            {"Name", new Slot {Name = "Name", Value = "daddy"}}
+                        }
+                    },
+                    Locale = "en-US",
+                    Timestamp = DateTime.Parse(AWSSDKUtils.FormattedCurrentTimestampISO8601)
+                },
+                Context = new Context
+                {
+                    AudioPlayer = new PlaybackState
+                    {
+                        PlayerActivity = "IDLE"
+                    },
+                    System = new AlexaSystem
+                    {
+                        Application = new Application
+                        {
+                            ApplicationId = "amzn1.ask.skill.4320cd87-2196-4cab-a608-431175fd1a84"
+                        },
+                        User = new User
+                        {
+                            UserId =
+                                "amzn1.ask.account.AHF37SX34WT3GSWLHWPCAREZX6AWL5RM5BE2HU3Y6CELX3VAB7PDMIQHZT3R3OZJHC6BM44NIFBTQ2XKEGPICP4OA5T4JMTNXXVJI6GD3CXGOTYBATWXGIBPN5SYIN6EHOOLRKHSCKRE6POCBFDACIERZJ3WX5IPJJKXYQRTN74XXTUFLYEMR6GH2DWVKHMVLEMMFTWXEJHIUZA"
+                        },
+                        Device = new Device
+                        {
+                            SupportedInterfaces = new Dictionary<string, object>() { }
+                        }
+                    }
+                },
+                Version = "1.0",
+            };
         }
     }
 }
--- AlexaHelloSkill/Function.cs
+++ AlexaHelloSkill/Function.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -12,20 +12,76 @@ namespace AlexaHelloSkill
 {
     public class Function
     {
-        
+
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            var logger = context.Logger;
-            logger.Log("received : " + input);
-            return input != null && input.Equals("hello", StringComparison.CurrentCultureIgnoreCase)
-                ? input?.ToUpper()
-                : "Where are your manners";
+            var resp = new SkillResponse {Version = "1.0"};
+            if (input.GetRequestType() == typeof(LaunchRequest))
+            {
+                resp.Response = new ResponseBody
+                {
+                        Card = new SimpleCard
+                        {
+                            Title = "Welcome",
+                            Content = "Postman welcomes you"
+                        },
+                        OutputSpeech = new PlainTextOutputSpeech
+                        {
+                            Text = "Postman Welcomes You."
+                        },
+                        ShouldEndSession = true
+                };
+            }
+            else if (input.GetRequestType() == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
+                var value = intentRequest.Intent.Slots.Values.FirstOrDefault(x => x.Name == "Name").Value;
+                if (value != null)
+                {
+                    var slot = value;
+                    switch (intentRequest.Intent.Name)
+                    {
+                        case "SendMessageIntent":
+
+                            resp.Response = new ResponseBody
+                            {
+                                Card = new SimpleCard
+                                {
+                                    Title = "Sent Message",
+                                    Content = "Sent Message to "+slot
+                                },
+                                OutputSpeech = new PlainTextOutputSpeech
+                                {
+                                    Text = "Sent message to "+slot
+                                },
+                                ShouldEndSession = true
+                            };
+                            break;
+                        case "HelloIntent":
+                            resp.Response = new ResponseBody
+                            {
+                                Card = new SimpleCard
+                                {
+                                    Title = "Hello",
+                                    Content = "Postman welcomes you"
+                                },
+                                OutputSpeech = new PlainTextOutputSpeech
+                                {
+                                    Text = "Postman welcomes you"
+                                },
+                                ShouldEndSession = true
+                            };
+                            break;
+                    }
+                }
+            }
+            return resp;
         }
     }
 }
